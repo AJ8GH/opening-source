43b7daa842763d4ae9e6274a7c3fd63f63daf9cd
Adam Jonas <adam.jonas@ppb.com>
2021-10-27 23:41:02 +0100

    Initial commit

---
+++ src/main/java/org/aj/openingsource/OpeningSourceApplication.java
@@ -0,0 +1,13 @@
+package org.aj.openingsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpeningSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpeningSourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/aj/openingsource/OpeningSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aj.openingsource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpeningSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
